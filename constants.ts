4fb28a5be1eb97ff96fd0aa87397e16d6dfb2461
Alexey Krukovich <aliaksei.krukovich@itechart-group.com>
2022-06-13 13:58:26 +0300

    feat: add status code constants

--- constants.ts
+++ constants.ts
@@ -1,3 +1,15 @@
-export const STATUS_CODE = Object.freeze({
+export const STATUS_CODE: Readonly<{
+  OK: 200;
+  CREATED: 201;
+  NO_CONTENT: 204;
+  BAD_REQUEST: 400;
+  NOT_FOUND: 404;
+  INTERNAL_SERVER_ERROR: 500;
+}> = Object.freeze({
   OK: 200,
+  CREATED: 201,
+  NO_CONTENT: 204,
+  BAD_REQUEST: 400,
+  NOT_FOUND: 404,
+  INTERNAL_SERVER_ERROR: 500,
 });
